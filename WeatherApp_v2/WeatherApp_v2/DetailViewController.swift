8f7f1499d1e8d20a5ec7f53da397f40de8783edb
sebastians96 <skoczen.kontakt@gmail.com>
2020-06-16 20:39:38 +0200

    Second project init in this repo

---
+++ WeatherApp_v2/WeatherApp_v2/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  WeatherApp_v2
+//
+//  Created by Student on 16.06.2020.
+//  Copyright © 2020 agh. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
